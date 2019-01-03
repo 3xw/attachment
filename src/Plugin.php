0be3898cf5267a6894d51b09d2de4606ce3b0a9d
Wallef Antoine <antoine.wallef@gmail.com>
2019-01-03 12:36:49 +0100

    plugin new style

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,7 +2,17 @@
 namespace Attachment;
 
 use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
 
 class Plugin extends BasePlugin
 {
+  public function bootstrap(PluginApplicationInterface $app)
+  {
+    parent::bootstrap($app);
+  }
+
+  public function routes($routes)
+  {
+    parent::routes($routes);
+  }
 }
