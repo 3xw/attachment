7fc004c8b63c690644fc72f3b37decb1c7928447
Wallef Antoine <antoine.wallef@gmail.com>
2019-08-12 16:48:00 +0200

    virtual fields

--- src/Model/Entity/Attachment.php
+++ src/Model/Entity/Attachment.php
@@ -8,19 +8,12 @@ use Cake\Routing\Router;
 class Attachment extends Entity
 {
 
-  /**
-  * Fields that can be mass assigned using newEntity() or patchEntity().
-  *
-  * Note that when '*' is set to true, this allows all unspecified fields to
-  * be mass assigned. For security purposes, it is advised to set '*' to false
-  * (or remove it), and explicitly make individual fields accessible as needed.
-  *
-  * @var array
-  */
   protected $_accessible = [
     '*' => true,
     'id' => false,
   ];
+
+  protected $_virtual = ['fullpath','mime'];
 
   protected function _getFullpath()
   {
@@ -30,7 +23,7 @@ class Attachment extends Entity
     return $baseUrl.$this->path;
   }
 
-  protected function _getUrl()
+  protected function _getMime()
   {
     return $this->_properties['type'].'/'.$this->_properties['subtype'];
   }
