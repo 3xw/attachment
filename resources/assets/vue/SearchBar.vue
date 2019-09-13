4b498f1ec43d15bbd235dcc75d24aa07ca087a1a
jeanvier <damien.grossfeld@gmail.com>
2019-09-13 10:55:05 +0200

    new SearchBar

---
+++ resources/assets/vue/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <label class="sr-only" for="inlineFormInputGroup">Username</label>
+      <div class="input-group mb-2">
+        <input v-model="needle" @keyup.enter="search" type="text" class="form-control" id="inlineFormInputGroup" placeholder="RECHERCHE">
+        <div type="submit" class="input-group-append">
+          <div class="input-group-text">
+            <i class="material-icons">search</i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
+export default
+{
+  name:'attachment-search-bar',
+  props: { aid: String },
+  data()
+  {
+    return {
+      needle: ''
+    }
+  },
+  methods:
+  {
+    search()
+    {
+      this.$store.set(this.aid + '/aParams', Object.assign(this.$store.get(this.aid + '/aParams'),{ search: this.needle, page: 1 }))
+    }
+  }
+}
+</script>
