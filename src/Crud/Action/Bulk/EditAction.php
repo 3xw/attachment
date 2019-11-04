885e43fd8e472173d07df01b91668511d26c5cef
awallef <antoine.wallef@gmail.com>
2019-11-04 10:58:07 +0100

    test edit

--- src/Crud/Action/Bulk/EditAction.php
+++ src/Crud/Action/Bulk/EditAction.php
@@ -5,38 +5,27 @@ namespace Attachment\Crud\Action\Bulk;
 
 use Cake\Controller\Controller;
 use Cake\ORM\Query;
-use Crud\Action\Bulk\BaseAction;
 
-class EditAction extends BaseAction
+class EditAction extends BaseJsonRestAction
 {
-  public function __construct(Controller $Controller, array $config = [])
-  {
-    $this->_defaultConfig['messages'] = [
-      'success' => [
-        'text' => 'Edit completed successfully',
-      ],
-      'error' => [
-        'text' => 'Could not complete edit',
-      ],
-    ];
-
-    parent::__construct($Controller, $config);
-  }
-
   protected function _bulk(?Query $query = null): bool
   {
     // retrieve
     $associated = $this->getConfig('relatedModels')?? [];
     $query->contain($associated);
-    $list = $query->toArray();
+    $indexedList = $query->toArray();
 
     // patch
-    $patched = $this->_table()->patchEntities(
-      $list,
-      $this->_controller()->getRequest()->getData(),
-      ['associated' => $associated ]
-    );
-
+    $patched = [];
+    foreach($indexedList as $pk => $entity)
+    {
+      $patched[] = $this->_table()->patchEntities(
+        $entity,
+        $this->subject->data[$pk],
+        ['associated' => $associated ]
+      );
+    }
+    
     // save
     return $this->_table()->saveMany($patched, ['associated' => $associated ]);
   }
