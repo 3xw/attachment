dd9fbfd09090224aa75b7ca61666ef3b624b00ab
awallef <antoine.wallef@gmail.com>
2019-11-15 16:14:10 +0100

    SNS tout bon

--- src/Filesystem/Compressor/LambdaCompressor.php
+++ src/Filesystem/Compressor/LambdaCompressor.php
@@ -1,30 +1,32 @@
 <?php
 namespace Attachment\Filesystem\Compressor;
 
-use Cake\Core\InstanceConfigTrait;
-use Cake\Http\ServerRequest;
-use Attachment\Model\Entity\Aarchive;
 use Cake\Event\Event;
+use Aws\Sns\SnsClient;
+use Aws\Exception\AwsException;
+use Cake\Http\Exception\BadRequestException;
 
 class LambdaCompressor extends BaseCompressor
 {
-  use InstanceConfigTrait;
-
-  protected $_defaultConfig = [];
-
-  public function __construct(array $config = [])
-  {
-    $this->setConfig($config);
-  }
-
-  public function beforeSave(Event $event): void
-  {
-    $event->getSubject()->entity->set('state', 'CREATED');
-    if($event->getSubject()->entity->hasErrors()) throw new \Exception("Error Processing Request", 1);
-  }
-
   public function afterSave(Event $event): void
   {
-    if(!$event->getSubject()->success) throw new \Exception("Error Processing Request", 1);
+    if(!$event->getSubject()->success) return;
+
+    $entity = $event->getSubject()->entity;
+    $SnSclient = new SnsClient($this->getConfig('clientSettings'));
+    $message = json_encode(array_merge(['id' => $entity->id], $this->getConfig('data')));
+    $topic = $this->getConfig('TopicArn');
+
+    // send message
+    try {
+      $result = $SnSclient->publish([
+        'Message' => $message,
+        'TopicArn' => $topic,
+      ]);
+    } catch (AwsException $e)
+    {
+      // output error message if fails
+      throw new BadRequestException($e->getMessage());
+    }
   }
 }
