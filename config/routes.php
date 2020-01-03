d282634051bc7e0deb6d119fb300354e07c994c2
awallef <antoine.wallef@gmail.com>
2020-01-03 14:37:34 +0100

    more

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@ use Cake\Routing\Router;
 // set thumbnails route
 Router::connect('/thumbnails/*', ['prefix' => false, 'plugin' => 'Attachment', 'controller' => 'Resize', 'action' => 'proceed']);
 
-Router::prefix('admin', function (RouteBuilder $routes) {
+Router::prefix('Admin', function (RouteBuilder $routes) {
 	$routes->plugin('Attachment', function (RouteBuilder $routes) {
 		$routes->fallbacks();
 	});
