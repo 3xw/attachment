9cd5d3bdbd681499eb5dc3fca9b8dd7aca1993b7
awallef <antoine.wallef@gmail.com>
2020-01-30 11:50:43 +0100

    cleanup

--- src/Model/Entity/Attachment.php
+++ src/Model/Entity/Attachment.php
@@ -11,21 +11,15 @@ class Attachment extends Entity
   protected $_virtual = ['mime','url','thumb_params'];
   protected function _getUrl()
   {
-    if(empty($this->_properties['profile']) || empty($this->_properties['profile'])) return '';
-
     return ProfileRegistry::retrieve($this->profile)->getUrl($this->path);
   }
   protected function _getThumbParams()
   {
-    if(empty($this->_properties['profile']) || empty($this->_properties['profile'])) return '';
-
     $url = ProfileRegistry::retrieve($this->profile)->thumbProfile()->getUrl($this->path);
     return strrpos($url, '?') === false ? '': substr($url, strrpos($url, '?'));
   }
   protected function _getFullpath()
   {
-    if(empty($this->_properties['profile']) || empty($this->_properties['profile'])) return '';
-    
     return ProfileRegistry::retrieve($this->profile)->getFullPath($this->path);
   }
   protected function _getMime()
