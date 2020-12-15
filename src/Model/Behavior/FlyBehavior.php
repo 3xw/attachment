3ebabc28759b4b455865caaa9d54af427d3bf5e2
awallef <antoine.wallef@gmail.com>
2020-12-15 11:41:52 +0100

    orientation

--- src/Model/Behavior/FlyBehavior.php
+++ src/Model/Behavior/FlyBehavior.php
@@ -117,6 +117,7 @@ class FlyBehavior extends Behavior
         // add image meta
         if(in_array($this->_file['type'], ['image/jpeg','image/png','image/gif']))
         {
+          if($this->_file['type'] == 'image/jpeg') $this->_image_fix_orientation($this->_file['tmp_name']);
           $image_info = getimagesize($this->_file['tmp_name']);
           $image_width = $image_info[0];
           $image_height = $image_info[1];
@@ -158,6 +159,30 @@ class FlyBehavior extends Behavior
         // excute callback fct if needed
         if(is_callable($afterReplace) && !empty($orginalValues[$field]) && $profile->replaceExisting) $afterReplace($entity);
       }
+    }
+  }
+
+  private function _image_fix_orientation($filename)
+  {
+    $exif = exif_read_data($filename);
+    if (!empty($exif['Orientation']))
+    {
+      $image = imagecreatefromjpeg($filename);
+      switch ($exif['Orientation'])
+      {
+        case 3:
+        $image = imagerotate($image, 180, 0);
+        break;
+
+        case 6:
+        $image = imagerotate($image, -90, 0);
+        break;
+
+        case 8:
+        $image = imagerotate($image, 90, 0);
+        break;
+      }
+      imagejpeg($image, $filename, 90);
     }
   }
 
